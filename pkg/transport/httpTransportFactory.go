ca4c3e04ba0aba14f6573c8b20a212b974847285
hector <hec@ungravity.net>
2019-01-30 14:18:04 +1100

    New defaults

--- pkg/transport/httpTransportFactory.go
+++ pkg/transport/httpTransportFactory.go
@@ -14,7 +14,11 @@ type HttpTransportFactory interface {
 var DefaultHttpTransportFactory HttpTransportFactory = &httpTransportFactory{
 	dialer: &net.Dialer{
 		Timeout:   10 * time.Second,
-		KeepAlive: 30 * time.Second,
+
+		// KeepAlive: 30 * time.Second,
+		// New settings because we use one new transport connection per request
+		KeepAlive: 0 * time.Second,
+
 		DualStack: true,
 	},
 }
@@ -26,8 +30,13 @@ type httpTransportFactory struct {
 
 func (f *httpTransportFactory) NewTransport() *http.Transport  {
 	return &http.Transport{
-		MaxIdleConns:           10,
-		IdleConnTimeout:        90 * time.Second,
+
+		//MaxIdleConns:           10,
+		// IdleConnTimeout:        90 * time.Second,
+		// New settings because we use one new transport per request
+		MaxIdleConns:           1,
+		IdleConnTimeout:        5 * time.Second,
+
 		TLSHandshakeTimeout:    10 * time.Second,
 		ExpectContinueTimeout:  1 * time.Second,
 		DialContext: 			f.dialer.DialContext,
